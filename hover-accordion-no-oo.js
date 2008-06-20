f83dafd6ae5b9e62d70c70efdf44fbfafbd9bdb7
Thomas Reynolds <tdreyno@gmail.com>
2008-06-19 17:23:36 -0700

    my style

--- hover-accordion-no-oo.js
+++ hover-accordion-no-oo.js
@@ -10,14 +10,15 @@ function hoverAccordion(list) {
 		duration:    350       // Sliding time in milliseconds
 	}, arguments[1] || {});
   
-  var effect = {
-    toggle: function (showElem, hideElem, afterCallback) {
+	// Reference action method when given an alias
+	if (!options.effect) {
+    function toggleEffect(showElem, hideElem, afterCallback) {
       hideElem.hide();
       showElem.show();
       afterCallback();
-    },
-    
-    slide: function (showElem, hideElem, afterCallback) {
+    }
+  
+    function slideEffect(showElem, hideElem, afterCallback) {
       var parallel = new Effect.Parallel([
         Effect.BlindDown(showElem),
         Effect.BlindUp(hideElem)
@@ -26,23 +27,19 @@ function hoverAccordion(list) {
         afterFinish: afterCallback
       });
     }
-  };
   
-	// Reference action method when given an alias
-	if (!options.effect) {
-    options.effect = (options.duration <= 0) ? effect.toggle : effect.slide;
+    options.effect = (options.duration <= 0) ? toggleEffect : slideEffect;
   }
   
   function beginSlide(clickedElement) {
-    if (isSliding || (overElement === currentElement) || (overElement !== clickedElement)) {
+    if (isSliding || (overElement === currentElement) || (overElement !== clickedElement))
       return;
-    }
 
     isSliding = true;
     currentElement.removeClassName(options.activeClass);
     clickedElement.addClassName(options.activeClass);
     
-    options.effect(clickedElement.next('dd'), currentElement.next('dd'), function (clickedElement) {
+    options.effect(clickedElement.next('dd'), currentElement.next('dd'), function(clickedElement) {
       isSliding      = false;
       currentElement = clickedElement;
     }.curry(clickedElement));
@@ -50,26 +47,23 @@ function hoverAccordion(list) {
   
   function setup() {
     elem = $(elem);
-    elem.select('dt + dd').each(function (e) {
+    elem.select('dt + dd').each(function(e) {
       var header = e.previous('dt');
       header.setAttribute('hascontent', 'true');
-      if (header.hasClassName(options.activeClass)) {
+      if (header.hasClassName(options.activeClass))
         currentElement = header;
-      } else {
+      else
         e.hide();
-      }
     });
 
-    document.observe('mousemove', function (event) {
-      if (isSliding) {
+    document.observe('mousemove', function(event) {
+      if (isSliding)
         return;
-      }
 
       overElement = event.findElement('dt');
 
-      if (!overElement || (overElement.getAttribute('hascontent') !== 'true') || (overElement.up('dl') !== elem)) {
+      if (!overElement || (overElement.getAttribute('hascontent') !== 'true') || (overElement.up('dl') !== elem))
         return;
-      }
 
       // wait a few seconds, if still over elem, start slide
       setTimeout(beginSlide.curry(overElement), options.wait);
@@ -77,9 +71,8 @@ function hoverAccordion(list) {
   }
   
   // Wait until the DOM is loaded
-  if (document.loaded) {
+  if (document.loaded)
     setup(elem);
-  } else {
+  else
     document.observe('dom:loaded', setup);
-  }
 }
