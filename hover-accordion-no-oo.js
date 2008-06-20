2b845c0f2ab939d95bbfc1719b4ded9e8870369e
Thomas Reynolds <tdreyno@gmail.com>
2008-06-19 17:00:52 -0700

    follow jslint on non-oo version.

--- hover-accordion-no-oo.js
+++ hover-accordion-no-oo.js
@@ -11,14 +11,14 @@ function hoverAccordion(list) {
 	}, arguments[1] || {});
   
   var effect = {
-    toggle: function(showElem, hideElem, afterCallback) {
+    toggle: function (showElem, hideElem, afterCallback) {
       hideElem.hide();
       showElem.show();
       afterCallback();
     },
     
-    slide: function(showElem, hideElem, afterCallback) {
-      new Effect.Parallel([
+    slide: function (showElem, hideElem, afterCallback) {
+      var parallel = new Effect.Parallel([
         Effect.BlindDown(showElem),
         Effect.BlindUp(hideElem)
       ], { 
@@ -29,51 +29,57 @@ function hoverAccordion(list) {
   };
   
 	// Reference action method when given an alias
-	if (!options.effect)
+	if (!options.effect) {
     options.effect = (options.duration <= 0) ? effect.toggle : effect.slide;
+  }
+  
+  function beginSlide(clickedElement) {
+    if (isSliding || (overElement === currentElement) || (overElement !== clickedElement)) {
+      return;
+    }
 
+    isSliding = true;
+    currentElement.removeClassName(options.activeClass);
+    clickedElement.addClassName(options.activeClass);
+    
+    options.effect(clickedElement.next('dd'), currentElement.next('dd'), function (clickedElement) {
+      isSliding      = false;
+      currentElement = clickedElement;
+    }.curry(clickedElement));
+  }
+  
   function setup() {
     elem = $(elem);
-    elem.select('dt + dd').each(function(e) {
+    elem.select('dt + dd').each(function (e) {
       var header = e.previous('dt');
       header.setAttribute('hascontent', 'true');
-      if (header.hasClassName(options.activeClass))
+      if (header.hasClassName(options.activeClass)) {
         currentElement = header;
-      else
+      } else {
         e.hide();
+      }
     });
 
-    document.observe('mousemove', function(event) {
-      if (isSliding)
+    document.observe('mousemove', function (event) {
+      if (isSliding) {
         return;
+      }
 
       overElement = event.findElement('dt');
 
-      if (!overElement || (overElement.getAttribute('hascontent') != 'true') || (overElement.up('dl') != elem))
+      if (!overElement || (overElement.getAttribute('hascontent') !== 'true') || (overElement.up('dl') !== elem)) {
         return;
+      }
 
       // wait a few seconds, if still over elem, start slide
       setTimeout(beginSlide.curry(overElement), options.wait);
     });
   }
   
-  function beginSlide(clickedElement) {
-    if (isSliding || (overElement == currentElement) || (overElement != clickedElement))
-      return;
-
-    isSliding = true;
-    currentElement.removeClassName(options.activeClass);
-    clickedElement.addClassName(options.activeClass);
-    
-    options.effect(clickedElement.next('dd'), currentElement.next('dd'), function(clickedElement) {
-       isSliding      = false;
-       currentElement = clickedElement;
-     }.curry(clickedElement));
-  }
-  
   // Wait until the DOM is loaded
-  if (document.loaded)
+  if (document.loaded) {
     setup(elem);
-  else
+  } else {
     document.observe('dom:loaded', setup);
+  }
 }
